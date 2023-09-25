5620ced6dcbe072e165ccbd06fd7fb9f552a4fb1
bammytoye <bamigbalatoyese@gmail.com>
2023-09-25 18:59:21 +0100

    button effect v.one

---
+++ button.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('a');
+
+btn.addEventListener('mousemove', e => {
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX * 3 - rect.left;
+    btn.style.setProperty('--x', x + 'deg');
+})
